202d1915236a497b752835fe653de52ee2db8f05
Snow02203835 <snow02203835@163.com>
2021-10-17 18:53:12 +0800

    力扣P0230-二叉搜索树中第k小的元素(已完成)

--- LeetCode/src/code/P0230.java
+++ LeetCode/src/code/P0230.java
@@ -8,29 +8,50 @@ import model.TreeNode;
  * @since 2021/10/17 18:23
  */
 public class P0230 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(8,
+                new TreeNode(4,
+                        new TreeNode(2,
+                                new TreeNode(1),
+                                new TreeNode(3)),
+                        new TreeNode(6,
+                                new TreeNode(5),
+                                new TreeNode(7))),
+                new TreeNode(12,
+                        new TreeNode(10,
+                                new TreeNode(9),
+                                new TreeNode(11)),
+                        new TreeNode(14,
+                                new TreeNode(13),
+                                new TreeNode(15))));
+        P0230 main = new P0230();
+        for (int i = 1; i < 16; i++) {
+            System.out.println(main.kthSmallest(root, i));
+        }
+    }
     public int kthSmallest(TreeNode root, int k) {
-        return -find(root, 1, k);
+        return -find(root, 1, k) - 100;
     }
     private int find(TreeNode root, int curr, int k) {
         if (root.left != null) {
             curr = find(root.left, curr, k);
             if (curr < 0) {
                 return curr;
+            } else {
+                curr++;
             }
         }
+        int temp = curr;
         if (root.right != null) {
-            int temp = find(root.right, curr, k);
+            temp = find(root.right, curr + 1, k);
             if (temp < 0) {
                 return temp;
             }
         }
-        if (root.left == null && root.right == null) {
-            curr++;
-        }
         if (curr == k) {
-            return -root.val;
+            return -(root.val + 100);
         } else {
-            return curr;
+            return temp;
         }
     }
 }
