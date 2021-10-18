0fc6fe8e8db79d5d4d07032e2c1e3ee4cf9c0ab1
Snow02203835 <snow02203835@163.com>
2021-10-18 14:20:21 +0800

    力扣P0690-员工的重要性

---
+++ LeetCode/src/code/P0690.java
@@ -0,0 +1,35 @@
+package code;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 员工的重要性
+ * @author snow
+ * @since 2021/10/18 14:15
+ */
+public class P0690 {
+    public int getImportance(List<Employee> employees, int id) {
+        HashMap<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        LinkedList<Employee> list = new LinkedList<>();
+        list.add(map.get(id));
+        int importance = 0;
+        while (!list.isEmpty()) {
+            Employee mine = list.pollFirst();
+            importance += mine.importance;
+            for (Integer integer : mine.subordinates) {
+                list.add(map.get(integer));
+            }
+        }
+        return importance;
+    }
+    static class Employee {
+        public int id;
+        public int importance;
+        public List<Integer> subordinates;
+    };
+}
